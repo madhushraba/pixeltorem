405ad4b655bc748edc9553ebb71c342c1bc0b514
Madhushraba <madhushraba21das@gmail.com>
2023-05-05 19:38:25 +0530

    pixeltorem converter

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import "./PixelToRemConverter.css";
 
-function PixelToRemConverter() {
-  const [pixelValue, setPixelValue] = useState('');
-  const [remValue, setRemValue] = useState('');
+const PixelToRemConverter = () => {
+  // Set the initial pixel value to 16
+  const [pixelValue, setPixelValue] = useState(16);
 
-  const handlePixelChange = (e) => {
-    const value = e.target.value;
-    setPixelValue(value);
-    setRemValue(`${value / 16}rem`); // assuming 1rem = 16px
+  // Calculate the rem value based on the current pixel value
+  const remValue = `${(pixelValue / 16).toFixed(3)}rem`;
+
+  // Update the pixel value when the user types in the input field
+  const handlePixelChange = (event) => {
+    const value = parseInt(event.target.value, 10);
+
+    if (isNaN(value)) {
+      setPixelValue("");
+    } else {
+      setPixelValue(value);
+    }
   };
 
+  // Render the pixel to rem converter
   return (
-    <div>
-      <label htmlFor="pixel-input">Pixel Value:</label>
-      <input id="pixel-input" type="number" value={pixelValue} onChange={handlePixelChange} />
-
-      <label htmlFor="rem-input">Rem Value:</label>
-      <input id="rem-input" type="text" value={remValue} readOnly />
+    <div className="converter">
+      <label htmlFor="pixel-input">Pixel value:</label>
+      <input
+        id="pixel-input"
+        type="number"
+        min="0"
+        value={pixelValue}
+        onChange={handlePixelChange}
+      />
+      <div className="result">{remValue}</div>
     </div>
   );
-}
+};
+
+export default PixelToRemConverter;
